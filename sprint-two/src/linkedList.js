662e0c2401beb61c4596b1c1287e75a20db1adc1
Daniel <ramossanchezd@gmail.com>
2016-01-16 18:27:15 -0800

    Finished linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,32 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    var newNode = Node(value);
+    //if this first node, set head and tail to newNode
+    if (list.head === null && list.tail === null) {
+      list.head = newNode;
+      list.tail = newNode;
+    } else {
+      list.tail.next = newNode;
+      list.tail = newNode;
+    }
   };
 
   list.removeHead = function(){
+    //cache the value of the first node, then delete the node
+    var oldHeadVal = list.head.value;
+    list.head = list.head.next;
+    //and return the cached node
+    return oldHeadVal;
   };
 
   list.contains = function(target){
+    for(var key in list){
+      if(list[key]['value']===target){
+        return true;
+      }
+    }
+    return false;
   };
 
   return list;
