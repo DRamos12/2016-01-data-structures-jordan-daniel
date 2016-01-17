89392b30126dbcb7ddb9ad5bdfd7cf6f5c3f4290
Daniel <ramossanchezd@gmail.com>
2016-01-16 18:27:40 -0800

    Finished set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,27 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	if(typeof item === 'string'){
+    //testing to make sure the item does not exist already
+    if(!_.contains(this._storage, item)){
+		  this._storage.push(item);
+    }
+  }
 };
 
 setPrototype.contains = function(item){
+	return _.contains(this._storage, item);
 };
 
 setPrototype.remove = function(item){
+  var index = _.indexOf(this._storage, item);
+  this._storage.splice(index, 1); 
 };
 
 /*
